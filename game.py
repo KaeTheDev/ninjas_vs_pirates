491b72ba9b4ca4bb9a37bf5f5a4b78618b305266
Andrew T <me@andrewtran-shi.dev>
2023-06-07 13:02:44 -0700

    renamed to work now

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
-from classes.ninja import Ninja
-from classes.pirate import Pirate
 import random
 
+from classes.ninja import Ninja
+from classes.pirate import Pirate
 
-#create players
+# create players
 sensei = Ninja("Michelanglo")
 jack_sparrow = Pirate("Jack Sparrow")
 
@@ -12,21 +12,12 @@ rounds = 0
 while sensei.health > 0 and jack_sparrow.health > 0:
     player_input = ""
     while not player_input == "1" and not player_input == "2":
-    # Using the input method, we can request something from the user in the terminal.
+        # Using the input method, we can request something from the user in the terminal.
         player_input = input("What to do?\n 1) Attack\n 2) Use skill\n <=========> ")
     if player_input == "1":
         sensei.punchAttack(jack_sparrow)
     elif player_input == "2":
         jack_sparrow.drinkWhiskey(Pirate)
-
-
-
-
-
-
-
-
-
 
 
 print("Both players intial stats: ")
@@ -41,7 +32,5 @@ print("\n==================\n")
 print("ROUND ONE")
 
 # USE THE SPEED TO DECIDE WHO GOES FIRST
-#michelangelo.attack(jack_sparrow)
+# michelangelo.attack(jack_sparrow)
 jack_sparrow.drinkWhiskey("Crown Royal")
-
-
